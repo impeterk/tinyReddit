27c9b52366f0a1f66b8d3e7ef75c4634b6a36c51
peter <peterkudelas@gmail.com>
2022-10-26 22:32:41 +0200

    cleaned up App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState, } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Search from './Search'
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <div className="App">
     <Search />
