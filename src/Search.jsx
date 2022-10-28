1ba5649d425bad2ec3f8713cc4b003deb6569941
peter <peterkudelas@gmail.com>
2022-10-28 18:32:38 +0200

    fixed problem with no search results crashing site

--- src/Search.jsx
+++ src/Search.jsx
@@ -14,7 +14,6 @@ export default function Search(props) {
             let response = await fetch(`/api${subredditOnly ? `/r/${subreddit}/` : `/`}search.json?q=${search}`)
             let responseJSON = await response.json()
             setSearchResults(responseJSON)
-            console.log(subredditOnly)
         }
         showSearchResults()
     },[search, subredditOnly])
@@ -37,7 +36,6 @@ export default function Search(props) {
     const handleSearch = event => {
         setSearch(event.target.value)
     }
-
     return(
         <div>
             <h1>{subreddit.toLocaleUpperCase()}</h1>
@@ -58,16 +56,18 @@ export default function Search(props) {
         <input 
         type="checkbox"
         id="subredditSearch"
-        checked={subredditOnly ? true : false}
+        defaultChecked={false}
         onClick={() => setSubredditOnly(checked => !checked)}
         />
         <label htmlFor="subredditSearch">subreddit Search</label>
         {search == '' ? <Data subreddit={subreddit} />: (
+        searchResults ? 
         <ol>
         {searchResults.data.children.map(post => (
           <li key={post.data.id}><p>{post.data.subreddit}</p><br/><a href={`https://reddit.com${post.data.permalink}`} target="_blank">{post.data.title}</a></li>
         ))}
       </ol>
+      : <p>No results</p>
       )}
         </div>
     )
