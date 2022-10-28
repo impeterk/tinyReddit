8ac700fb3e0f3d1625693c6958147181da0751ef
peter <peterkudelas@gmail.com>
2022-10-28 21:31:54 +0200

    created new <SearchResult /> component

---
+++ src/SearchComponent.jsx
@@ -0,0 +1,19 @@
+export default function SearchComponent(props) {
+
+    if ((props.searchResults && props.searchResults.data.children.length == 0) || !props.searchResults) {
+        return (
+            <p>No Results</p>
+        )
+    }
+
+
+    return(
+        <div>
+            <ol>
+            {props.searchResults.data.children.map(post => (
+              <li key={post.data.id}><p>{post.data.subreddit}</p><br/><a href={`https://reddit.com${post.data.permalink}`} target="_blank">{post.data.title}</a></li>
+            ))}
+            </ol>
+        </div>
+    )
+}
