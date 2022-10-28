55b0d795e69f741600567c8c2310049adacd359d
peter <peterkudelas@gmail.com>
2022-10-28 18:18:34 +0200

    added search feature that displays content

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import {useState} from 'react'
 import Data from './Data'
 
@@ -5,11 +6,28 @@ export default function Search(props) {
     const [subreddit, setSubreddit] = useState('popular')
     const [value, setValue] = useState('')
     const [search, setSearch] = useState('')
+    const [subredditOnly, setSubredditOnly] = useState(false)
+    const [searchResults, setSearchResults] = useState(null)
+
+    useEffect(() => {
+        async function showSearchResults() {
+            let response = await fetch(`/api${subredditOnly ? `/r/${subreddit}/` : `/`}search.json?q=${search}`)
+            let responseJSON = await response.json()
+            setSearchResults(responseJSON)
+            console.log(subredditOnly)
+        }
+        showSearchResults()
+    },[search, subredditOnly])
+
 
     const handleSubmit = event => {
         event.preventDefault()
         setSubreddit(value)
         setValue('')
+        setSearchResults(null)
+        setSubredditOnly(false)
+        setSearch('')
+        document.getElementById('subredditSearch').checked = false
     }
 
     const handleChange = event => {
@@ -32,11 +50,25 @@ export default function Search(props) {
             <button>new subreddit</button>
         </form>
         <input
+        type="text"
         value={search}
         onChange={handleSearch}
         placeholder='Search'
         />
-        {search == '' ? <Data subreddit={subreddit} /> : <p>{search}</p>}
+        <input 
+        type="checkbox"
+        id="subredditSearch"
+        checked={subredditOnly ? true : false}
+        onClick={() => setSubredditOnly(checked => !checked)}
+        />
+        <label htmlFor="subredditSearch">subreddit Search</label>
+        {search == '' ? <Data subreddit={subreddit} />: (
+        <ol>
+        {searchResults.data.children.map(post => (
+          <li key={post.data.id}><p>{post.data.subreddit}</p><br/><a href={`https://reddit.com${post.data.permalink}`} target="_blank">{post.data.title}</a></li>
+        ))}
+      </ol>
+      )}
         </div>
     )
 }
