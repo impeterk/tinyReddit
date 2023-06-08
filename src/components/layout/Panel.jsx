d5abe0346cf46beccbab3fdf2e65aa3d354fa162
peter <peterkudelas@gmail.com>
2023-06-08 22:19:46 +0200

    cleaned up desktop panel

--- src/components/layout/Panel.jsx
+++ src/components/layout/Panel.jsx
@@ -5,25 +5,17 @@ export default function Panel() {
 
   return (
     <div className="container is-family-monospace">
-     <article className="panel is-sticky has-background-info is-flex is-flex-direction-column min-h-full is-fullheight is-info">
-  <p className="panel-heading">
- {`<React 2 Reddit />`} 
-  </p>
-  
-  <div className="panel-block">
-    <p className="control has-icons-left">
-      <input className="input is-info" type="text" placeholder="Search" />
-      <span className="icon is-left">
-        <i className="fas fa-search" aria-hidden="true"></i>
-      </span>
-    </p>
-    
-  </div>
-  <SubredditList />
-  <div className="mt-auto">
-<Footer />
-  </div>
-  </article> 
+      <article className="panel is-sticky has-background-info is-flex is-flex-direction-column min-h-full is-fullheight is-info">
+        <div className="panel-block">
+          <p className="panel-heading is-size-4-desktop">
+            {`<React 2 Reddit />`}
+          </p>
+        </div>
+        <SubredditList />
+        <div className="mt-auto">
+          <Footer />
+        </div>
+      </article>
     </div>
   )
 }
