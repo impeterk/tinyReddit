60eef9185512110ecffc72ead35ca36545ddfbf5
peter <peterkudelas@gmail.com>
2023-06-21 00:50:59 +0200

    added modal to for images

--- src/views/PostDetailView.jsx
+++ src/views/PostDetailView.jsx
@@ -1,6 +1,6 @@
 import { useParams, useNavigate } from "react-router-dom"
 import { getSubreddit } from "../api"
-import { useEffect } from "react"
+import { useEffect,useState} from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { loadPost, selectIsLoading, selectComments, selectPostData } from "../components/elements/postList/postListSlice"
 import Comment from "@/components/elements/postList/Comment"
@@ -15,7 +15,7 @@ export default function PostDetailView() {
 
     const postData = useSelector(selectPostData)
     const comments = useSelector(selectComments)
-
+    const [modal, setModal] = useState(false)
     useEffect(() => {
         dispatch(loadPost({ subreddit, id }))
     }, [id])
@@ -23,21 +23,25 @@ export default function PostDetailView() {
     function goBack() {
         navigate(-1)
     }
+    const toggleModal = () => {
+        setModal(() => {
+        return !modal
+        })
+    }
 
     if (isLoading) {
         return (
             <div className={`pageloader is-active is-light`}><span className="title">Loading...</span></div>
         )
     }
-
-
+    
     return (
         <div className="mt-4 ">
             <div className="card p-2 container">
                 <div className="card-content">
                     <div className="is-flex mb-4">
                         <button className="button is-info is-outlined" onClick={goBack}>
-                            <FontAwesomeIcon icon={faArrowLeft} size="lg" className="mr-2 "/>
+                            <FontAwesomeIcon icon={faArrowLeft} size="lg" className="mr-2 " />
                             Back</button>
                     </div>
                     <h1 className="title is-3">{postData.title}</h1>
@@ -46,9 +50,26 @@ export default function PostDetailView() {
                 {postData.post_hint == "image" ?
                     < div className="card-image">
                         <figure className="image">
-                            <img src={postData.url} alt={postData.title} />
+                            <img src={postData.url} onClick={toggleModal} alt={postData.title} />
                         </figure>
-                    </div> : ""}
+                        <div className={`modal ${modal ? 'is-active' : ''}`}>
+                            <div onClick={toggleModal} className="modal-background"></div>
+                            <div className="modal-content">
+                                <p className="image">
+                                    <img src={postData.url} alt="media image" />
+                                </p>
+                            </div>
+                            <button onClick={toggleModal} className="modal-close is-large" aria-label="close"></button>
+                        </div>
+                    </div>
+                    : ""}
+                {postData.media ?
+                    <div className="card-image">
+                        <video className="image is-clickable" controls>
+                            <source src={postData.media.reddit_video.fallback_url} type="video/mp4" />
+                        </video>
+                    </div>
+                    : ""}
                 <div className="card-content">
                     <div className="content">
                         {postData.selftext}
