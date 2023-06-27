8b2874865194ebba4e526edf5b673a2ecf367bce
peter <peterkudelas@gmail.com>
2023-06-27 18:18:20 +0200

    checked splling on imports

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import {configureStore} from '@reduxjs/toolkit'
 import subredditReducer from '/src/components/elements/navigation/subredditList/subredditSlice'
 import navBarMenuReducer from '/src/components/layout/navBar/navBarSlice'
 import postReducer from '/src/components/elements/postList/postListSlice'
-import searchReducer from '/src/components/elements/search/searchSlice'
+import searchReducer from '/src/components/elements/Search/searchSlice'
 export default configureStore({
     reducer:{
         search: searchReducer,
