65e1dd4cec5ec5c134804e1063893ecf5e6f99ed
Peter <peterkudelas@gmail.com>
2023-11-02 19:53:51 +0000

    new icon,  PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from  'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA({
+    manifest: {
+      icons: [
+        {
+          src: '/assets/react-reddit-logo.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable'
+        }
+      ]
+    }
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
